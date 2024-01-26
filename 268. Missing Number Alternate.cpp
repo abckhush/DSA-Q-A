e7ad371cf8b17b8634f813950fbc1fe17fed0ed6
Khushi <127378920+abckhush@users.noreply.github.com>
2024-01-26 23:42:32 +0530

    LeetCode 268 Alternate

--- 268. Missing Number Alternate.cpp
+++ 268. Missing Number Alternate.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int missingNumber(vector<int>& nums) {
+    int missingNumber(vector<int>& nums){
        int n = nums.size();
         int expectedSum = (n * (n + 1)) / 2;
         int actualSum = 0;
